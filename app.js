9f54595acef4c81f7460c083d5477be6d02ec4e3
Daniel Fernandez <daniel.fernandezgp@gmail.com>
2020-12-15 22:16:09 -0400

    This is my first comment

---
+++ app.js
@@ -0,0 +1,8 @@
+var messagejs = 'Hello, from JS';
+console.log(messagejs);
+var GREETINGS = 'Greeting';
+console.log(GREETINGS);
+var message = 'Hello, from TS';
+console.log(message);
+var heading = document.getElementById('greeting');
+heading.textContent = message;
---
+++ app.ts
@@ -0,0 +1,14 @@
+var messagejs = 'Hello, from JS';
+
+console.log(messagejs);
+
+const GREETINGS: string = 'Greeting'; 
+
+console.log(GREETINGS);
+
+let message: string = 'Hello, from TS';
+
+console.log(message);
+
+let heading = document.getElementById('greeting');
+heading.textContent = message;
